c4c31c98623c11fb69721faf889b204f5ea12b02
pradeep-k <pradeepk@gwmail.gwu.edu>
2015-09-28 00:26:50 -0400

    adding hw3 simple-serve and print-serve

--- lec-1/msgboard/python/msgclient.py
+++ lec-1/msgboard/python/msgclient.py
@@ -3,13 +3,46 @@
 # Run with:     python msgclient.py
 
 import socket
+import sys, getopt 
 
-#host = "twood02.koding.io";
-host = "127.0.0.1";
-portnum = 5555;
+def main(argv):
+    host = "127.0.0.1";
+    #host = "upkk5920aab9.pradeep-k.koding.io";
+    portnum = 5555;
+    name = "Pradeep";
+    message = "Hello World";
+    
+    try:
+        opts, args = getopt.getopt(argv,"h:s:p:n:m:")
+    except getopt.GetoptError:
+        print 'msgclient.py -s <server> -p <port> -n <name> -m <message>'
+        sys.exit()
 
-# your code here!
-clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-clientsocket.connect((host, portnum))
-clientsocket.send("Pradeep\nHello")
-print("Sent message to server!")
+    for opt, arg in opts:
+        if opt == '-h':
+            print 'msgclient.py -s <server> -p <port> -n <name> -m <message>'
+            sys.exit()
+        elif opt == '-s':
+            host = arg
+            #print host
+        elif opt == '-p':
+            portnum = arg
+            #print portnum
+        elif opt == '-n':
+            name = arg
+            #print name
+        elif opt == '-m':
+            message = arg
+            #print msg
+
+    # your code here!
+    clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    clientsocket.connect((host, portnum))
+    clientsocket.send(name)
+    clientsocket.send("\n")
+    clientsocket.send(message)
+
+    print("Sent message to server!")
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
---
+++ lec-3/discovery/protocols/simple-serve.c
@@ -0,0 +1,200 @@
+
+/**************************************************************************
+ *
+ *  CS 6421 - Simple Conversation
+ *      It sends out a welcome message when receive connection from client.
+ *  Compilation: gcc -o conv_server conv_server.c to compile
+ *  Execution:
+ *      server: ./conv_server portnum
+ *
+ **************************************************************************/
+//Maotong Xu
+//Pradeep K.
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <inttypes.h>
+#include <fcntl.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <errno.h>
+#include <getopt.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <math.h>
+
+
+
+static char* server_port;
+static char* server_ip;
+
+
+/*
+ * Print a usage message
+ */
+static void
+usage() {
+    printf("Usage:  ./server -p <my ip> -s <sever-ip> \n");
+}
+
+static void handle_get(char* tokens) 
+{
+    printf("handling get\n");
+}
+
+static void handle_set(char* tokens)
+{
+    printf("handling set\n");
+}
+
+// processing function
+void
+process(int sock)
+{
+    int n;
+    int BUFSIZE = 1024;
+    char buf[BUFSIZE];
+    char* msg = "Welcome, you are connected to a C-based server\n";
+    char* userInput;
+
+    /* Write a welcome message to the client */
+    n = write(sock, msg, strlen(msg));
+
+    if (n < 0){
+        perror("ERROR writing to socket");
+        exit(1);
+    }
+
+    /* read and print the client's request */
+    bzero(buf, BUFSIZE);
+    n = read(sock, buf, BUFSIZE);
+    if (n < 0){
+        perror("ERROR reading from socket");
+        exit(1);
+    }
+    userInput = buf;
+    close(sock);
+    printf("Received message: %s\n", userInput);
+
+    const char s[2] = " ";				//space as delimiter
+    char *token = 0;
+    /* get the first token */
+    token = strtok(userInput, s);
+    /* walk through other tokens */
+    /*
+    int i = 0;
+    while( token[i] != NULL ) {
+      i++;
+      token[i] = strtok(NULL, s);
+    }
+    */
+    if (strcmp(token, "get")) {
+        handle_get(userInput );
+    } else if (strcmp(token, "set")) {
+        handle_set(userInput);
+    }
+}
+   
+
+/*
+ * Server
+ */
+int
+server( void )
+{
+    int optval = 1;
+    int sockfd, newsockfd;
+    socklen_t clilen;
+    struct sockaddr_in serv_addr, cli_addr;
+    int  n;
+
+    /* First call to socket() function */
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof optval);
+
+    if (sockfd < 0){
+        perror("ERROR opening socket");
+        exit(1);
+    }
+    /* Initialize socket structure */
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(atoi(server_port));
+    /* Now bind the host address using bind() call.*/
+    if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0){
+        perror("ERROR on binding");
+        exit(1);
+    }
+    /* Listening for the client */
+    listen(sockfd,5);
+    clilen = sizeof(cli_addr);
+
+    /* Loop forever receiving client connections */
+    while(1) {
+        /* Accept connection from the client */
+        newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
+        if (newsockfd < 0){
+            perror("ERROR on accept");
+            exit(1);
+        }
+        printf("Accepted connection from client\n");
+        /* Process a client request on new socket */
+        process(newsockfd);
+    }
+
+    /*clean up*/
+    close(sockfd);
+
+    return 0;
+}
+
+int main(int argc, char ** argv)
+{
+
+    const char* progname = argv[0];
+    //char* server_port   = "5555";
+    //char* server_ip     = "upkk5920aab9.pradeep-k.koding.io";
+        
+    int o;
+        
+    while ((o = getopt (argc, argv, "hp:s:")) != -1) {
+            switch(o){
+            case 's':
+                    server_ip = optarg;
+                    break;
+            case 'h':
+                    usage();
+                    return 0;
+            case 'p':
+                    server_port = optarg;
+                    break;
+            case '?':
+                    if (optopt == 'h') { 
+                            usage();
+                            return 0;
+                    } else if(optopt == 'p' || optopt == 's') {
+                            fprintf (stderr, "Option %c requires an argument.\n", optopt);
+                     
+                    } else {
+                            fprintf (stderr, "Unknown argument: %c.\n", optopt);
+                    }
+                    return -1;
+            }
+    }
+    if (argc == 1) {
+        usage();
+        return 0;
+    } 
+     
+    printf("server_ip: %s port: %s\n", server_ip, server_port);
+	
+
+    //TODO: add arguments exception handling here
+	
+    if (server() != 0){
+        printf("server in trouble\n");
+        exit(1);
+    }
+}
